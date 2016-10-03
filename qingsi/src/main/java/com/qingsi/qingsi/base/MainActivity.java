79bc608c067c495d1b9c6a4dc08e8fea86767398
Administrator <15737939317@163.com>
2016-10-03 10:10:04 +0800

    四个Fragment加载不同内容

--- qingsi/src/main/java/com/qingsi/qingsi/base/MainActivity.java
+++ qingsi/src/main/java/com/qingsi/qingsi/base/MainActivity.java
@@ -2,8 +2,16 @@ package com.qingsi.qingsi.base;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.qingsi.qingsi.R;
+import com.qingsi.qingsi.fujin.FragmentFujin;
+import com.qingsi.qingsi.siyu.FragmentSiyu;
+import com.qingsi.qingsi.tuijian.FragmentTuijian;
+import com.qingsi.qingsi.wode.FragmentWode;
 
 /**
  * 工作Activity，其他四个页面以Fragment形式加载
@@ -11,6 +19,8 @@ import com.qingsi.qingsi.R;
 public class MainActivity extends BaseActivity {
 
     private Context context;
+    private Fragment fragment = null;
+    TextView page_title;
 
 
     @Override
@@ -18,6 +28,46 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+
+    }
+
+    private void initView() {
+        page_title = (TextView) findViewById(R.id.page_title);
+
+        fragment = new FragmentTuijian();
+        load(fragment);
+
+        RadioButton radioButton_tuijian = (RadioButton) findViewById(R.id.radioButton_tuijian);
+        radioButton_tuijian.setChecked(true);
+    }
+
+    public void changeFragment(View view) {
+        switch (view.getId()) {
+            case R.id.radioButton_tuijian:
+                fragment = new FragmentTuijian();
+                page_title.setText("推荐");
+                break;
+            case R.id.radioButton_siyu:
+                fragment = new FragmentSiyu();
+                page_title.setText("思语");
+                break;
+            case R.id.radioButton_fujin:
+                fragment = new FragmentFujin();
+                page_title.setText("附近");
+                break;
+            case R.id.radioButton_wode:
+                fragment = new FragmentWode();
+                page_title.setText("我的");
+                break;
+            default:
+                break;
+        }
+        load(fragment);
+    }
+
+    private void load(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content,fragment).commit();
     }
 
 }
---
+++ qingsi/src/main/java/com/qingsi/qingsi/siyu/FragmentSiyu.java
@@ -0,0 +1,22 @@
+package com.qingsi.qingsi.siyu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/10/3 0003.
+ */
+public class FragmentSiyu extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("思语");
+        return textView;
+    }
+}
